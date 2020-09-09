416e3a8a70cadf921b671a0237af79d9c671beeb
Eudes Rodrigues <eron300@gmail.com>
2020-09-09 10:48:11 -0300

    first commit

---
+++ Aparecida nutrição/js/remover-paciente.js
@@ -0,0 +1,20 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick", function(event){
+
+	event.target.parentNode.classList.add("fadeOut");
+	
+	setTimeout(function(){
+		event.target.parentNode.remove();
+	},500);
+	
+});
+
+//pacientes.forEach(function(paciente){
+//	paciente.addEventListener("dblclick", function(){
+//		console.log("Fui clicado com um duplo click!");
+//		this.remove();
+//	});
+//});
